fc899fc1574471729f80a0326ad098f6e6036b98
Frank Cary <fcaryv@gmail.com>
2020-10-25 17:09:51 -0500

    [EC-xxx] Up: Update sh file

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/home/ec2-user/td-site/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ec2-user/td-site/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP nginx
+$COMPOSE run certbot renew  && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
